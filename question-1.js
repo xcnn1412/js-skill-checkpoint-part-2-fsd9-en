a40f5cfd4c2d4ce8eeb70cb24cca31caa6a895b2
napatwongchr <napat.wongchr@gmail.com>
2024-05-27 08:44:44 +0700

    init js skill checkpoint 2

---
+++ question-1.js
@@ -0,0 +1,14 @@
+const techupPeople = [
+  { name: "Alice", age: 17 },
+  { name: "Bob", age: 22 },
+  { name: "Charlie", age: 16 },
+];
+
+const techcoolPeople = [
+  { name: "David", age: 25 },
+  { name: "Eve", age: 19 },
+];
+
+const allPeople = [];
+
+// เริ่มเขียนโค้ดตรงนี้
---
+++ question-2.js
@@ -0,0 +1,9 @@
+const students = [
+  { name: "Alice", score: 42 },
+  { name: "Bob", score: 55 },
+  { name: "Charlie", score: 68 },
+  { name: "David", score: 48 },
+  { name: "Eve", score: 91 },
+];
+
+// เริ่มเขียนโค้ดตรงนี้
