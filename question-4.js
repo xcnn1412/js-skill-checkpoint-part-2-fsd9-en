eb86fcaa2bce21a9aa7b2d9d1a3b37299c101e48
En <graphic.mimage@gmail.com>
2025-08-05 14:56:49 +0700

    Fix Bug : Correct ' Engineering Department , Change Age filter to >= 20 , refactor.map() for the final output

--- question-4.js
+++ question-4.js
@@ -2,35 +2,51 @@ const alphaTech = [
     { name: 'Alice', age: 23, department: 'Engineering' },
     { name: 'Bob', age: 19, department: 'Design' }
   ];
-  
+
+  // console.log(`alphaTech` , alphaTech);
+
   const betaSoft = [
     { name: 'Charlie', age: 28, department: 'Engineering' },
     { name: 'David', age: 17, department: 'Support' }
   ];
   
+  // console.log(`betaSoft` , betaSoft);
+
   const gammaDev = [
     { name: 'Eve', age: 25, department: 'Marketing' },
     { name: 'Faythe', age: 18, department: 'Engineering' },
     { name: 'Grace', age: 20, department: 'Engineering' }
   ];
+
+  // console.log(`gammaDev` , gammaDev);
   
 
-const allEmployees = alphaTech.concat(betaSoft, gammaDev);
+//เติม company ลงไปใน allEmployees
+const allEmployees = [ 
+  ...alphaTech.map( addCom => ({ ...addCom, company: `alphaTech`})),
+  ...betaSoft.map( addCom => ({ ...addCom, company: `betaSoft`})),
+  ...gammaDev.map( addCom => ({ ...addCom, company: `gammaDev`}))
+];
 
-const result = allEmployees.filter((employee) => {
-  return employee.department === 'Enginering' && employee.age > 20;
-}).map((employee) => {
-  return {
-    name: employee.name,
-    company: '' 
-  };
-});
+console.log(`เติมCom` , allEmployees);
+
+//filter with Condition + map 
+const result = allEmployees
+  .filter( employee => employee.department === `Engineering` && employee.age >= 20)
+  .map ( employee => ({name: employee.name, company: employee.company}));
 
 console.log(result);
-// [
-//     { name: 'Alice', company: 'alphaTech' },
-//     { name: 'Charlie', company: 'betaSoft' },
-//     { name: 'Grace', company: 'gammaDev' }
-// ]
-  
+
+
+
+//Try Someting New
+// const result = [
+//     ...alphaTech.map( addCom => ({ ...addCom, company: `alphaTech`})),
+//     ...betaSoft.map( addCom => ({ ...addCom, company: `betaSoft`})),
+//     ...gammaDev.map( addCom => ({ ...addCom, company: `gammaDev`}))
+// ] 
+//     .filter ( employee => employee.department === `Engineering` && employee.age >= 20)
+//     .map( employee => ({name: employee.name , company: employee.company}));
+
+// console.log(result)
 
